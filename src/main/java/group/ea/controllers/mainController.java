5bfa7a04e2b12cb0785301db30f646b421f0727c
jakob_kild <s214952@dtu.dk>
2024-02-28 23:39:33 +0100

    init commit

--- src/main/java/group/ea/controllers/mainController.java
+++ src/main/java/group/ea/controllers/mainController.java
@@ -1,12 +1,18 @@
 package group.ea.controllers;
 
 import group.ea.main;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 import java.io.IOException;
@@ -14,12 +20,32 @@ import java.util.Objects;
 
 public class mainController {
     @FXML
-    private Button btnPlot, btnConnect, btnTable;
+    private Button btnPlot, btnConnect, btnTable, createBlueprintBtn, loadBlueprintBtn;
 
     @FXML
     private BorderPane mainBorderPane;
 
+    private Stage stage;
+    private Scene scene;
+    private Parent parent;
+    private FileChooser fileChooser = new FileChooser();
 
+
+    @FXML
+    void createBlueprintHandler(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(main.class.getResource("fxml/createBlueprintPage.fxml")));
+        Scene scene = new Scene(root);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        Platform.runLater(root::requestFocus);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void loadBlueprintHandler(ActionEvent event){
+        fileChooser.showOpenDialog(stage);
+    }
 
 
     @FXML
@@ -33,6 +59,11 @@ public class mainController {
         else if (event.getSource() == btnTable){
             changeContent("tablePage");
         }
+    }
+
+    @FXML
+    void closeProgram(ActionEvent event) {
+        Platform.exit();
     }
 
     private void changeContent(String page) throws IOException {
