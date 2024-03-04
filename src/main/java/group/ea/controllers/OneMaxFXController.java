eb3f28503d11c8c7ba66e6c13673aba1a80fe9ee
VicAceCase <victor.larsen5@hotmail.com>
2024-03-04 11:44:41 +0100

    (1+1)EA Basic working

--- src/main/java/group/ea/controllers/OneMaxFXController.java
+++ src/main/java/group/ea/controllers/OneMaxFXController.java
@@ -1,78 +1,12 @@
 package group.ea.controllers;
 
-import group.ea.main;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
+public class OneMaxFXController {
 
-import java.io.IOException;
-import java.util.Objects;
-
-public class mainController {
-    @FXML
-    private Button btnPlot, btnConnect, btnTable, createBlueprintBtn, loadBlueprintBtn;
-
-    @FXML
-    private BorderPane mainBorderPane;
-
-    private Stage stage;
-    private Scene scene;
-    private Parent parent;
-    private FileChooser fileChooser = new FileChooser();
-
-
-    @FXML
-    void createBlueprintHandler(ActionEvent event) throws IOException{
-        Parent root = FXMLLoader.load(Objects.requireNonNull(main.class.getResource("fxml/createBlueprintPage.fxml")));
-        Scene scene = new Scene(root);
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-
-        Platform.runLater(root::requestFocus);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    @FXML
-    void loadBlueprintHandler(ActionEvent event){
-        fileChooser.showOpenDialog(stage);
-    }
-
-
-    @FXML
-    void menuChangeHandler(ActionEvent event) throws IOException {
-        if (event.getSource() == btnPlot){
-            changeContent("plotPage");
-        }
-        else if (event.getSource() == btnConnect){
-            changeContent("connectPage");
-        }
-        else if (event.getSource() == btnTable){
-            changeContent("tablePage");
-        }
-    }
-
-    @FXML
-    void closeProgram(ActionEvent event) {
-        Platform.exit();
-    }
-
-    private void changeContent(String page) throws IOException {
-        Parent root = null;
-
-        root = FXMLLoader.load(Objects.requireNonNull(main.class.getResource("fxml/" + page + ".fxml")));
-        mainBorderPane.setCenter(root);
-    }
     @FXML
     private Button startButton;
     @FXML
